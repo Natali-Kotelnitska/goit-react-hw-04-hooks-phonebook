db8a46998221338d84e18b3336b09a720e36eec5
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-06-03 00:22:56 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import s from './ContactForm.module.css';
 
 const ContactForm = ({ onSubmit }) => {
